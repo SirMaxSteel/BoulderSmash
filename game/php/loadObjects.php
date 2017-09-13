7220b36aabda6082927360050589cfe63112797b
Klozilla <Klozilla@users.noreply.github.com>
2017-09-13 18:59:55 +0200

    bugfixes and added scoreline

---
+++ game/php/loadObjects.php
@@ -0,0 +1,45 @@
+﻿<?php
+
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+
+$directory = '../objects/';
+
+if(! is_dir($directory))
+{
+    exit('invalid directory');
+}
+
+$objects = array();
+
+
+//iterate through objects in folder
+
+$files = scandir($directory);
+
+for($i = 0; $i < count($files); $i++)
+{
+    $objectFile = $directory . $files[$i];
+
+    if($objectFile === '.' || $objectFile === '..' || !endsWith($objectFile, '.json'))
+        continue;
+        
+    $fileHandle = fopen($objectFile, 'r');
+    
+    $objects[] = json_decode(fread($fileHandle, filesize($objectFile)));
+    
+    fclose($fileHandle);     
+}
+
+echo json_encode($objects);
+
+
+?>
